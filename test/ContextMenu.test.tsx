229637382976bf65a8c51b3fec9b5511acfedd45
Adam Trager <we.have@unfinished.business>
2020-09-10 13:37:27 -0400

    testing and coverage

--- test/ContextMenu.test.tsx
+++ test/ContextMenu.test.tsx
@@ -1,27 +1,90 @@
 import React from 'react'
-import * as ReactDOM from 'react-dom'
-import { ContextMenu } from '../'
+
+import { render, fireEvent, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import { ContextMenu } from '../src'
+
 
 const Wrapper = () => {
   const containerRef = React.useRef(null)
   const items = [
     {
       children: 'Click me!',
+      'data-testid': 'menu-item'
     },
   ]
-
+  
   return (
-    <div ref={containerRef}>
-      Right click me!
-      <ContextMenu target={containerRef} items={items} />
-    </div>
+    <>
+      <div ref={containerRef} data-testid="wrapper">
+        Right click me!
+      </div>
+      <ContextMenu target={containerRef} data-testid="menu" items={items} />
+      <button data-testid="away">away</button>
+    </>
   )
 }
 
+const renderAndOpen = () => {
+  const renderData = render(<Wrapper />)
+  fireEvent.contextMenu(screen.getByTestId('wrapper'))
+
+  return renderData
+}
+
+const expectUnmounted = () => {
+  expect(screen.queryByTestId('menu')).toBeNull()
+}
+
+afterEach(cleanup)
+
 describe('Context Menu', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Wrapper />, div)
-    ReactDOM.unmountComponentAtNode(div)
+  describe('Mounting', () => {
+    it('should mount on right click', () => {
+      const { baseElement } = render(<Wrapper />)
+
+      expectUnmounted()
+
+      fireEvent.contextMenu(screen.getByTestId('wrapper'))
+
+      expect(screen.getByTestId('menu')).toBeInTheDocument()
+
+      expect(baseElement).toMatchSnapshot()
+    })
+  })
+
+  describe('Unmounting', () => {
+    beforeEach(() => {
+      renderAndOpen()
+    })
+
+    // it('should unmount on click away', () => {
+    //   fireEvent.click(screen.getByTestId('away'))
+
+    //   expectUnmounted()
+    // })
+  
+    it('should unmount on blur', () => {
+      fireEvent.blur(window)
+  
+      expectUnmounted()
+    })
+  })
+
+  describe('Children', () => {
+    it('should have one', () => {
+      renderAndOpen()
+
+      expect(screen.getByTestId('menu-item')).toBeInTheDocument()
+    })
+
+    it('should unmount after clicking one', () => {
+      renderAndOpen()
+
+      fireEvent.click(screen.getByTestId('menu-item'))
+
+      expectUnmounted()
+    })
   })
 })
